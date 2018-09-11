e919124e70eda7a05515666a97f569b9a28a0dbc
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-11 14:07:39 -0700

    Adds tasks to list via onClick hook

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
   styleUrls: [
-    './shopping-list.component.css',
-    '../../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css'
+    "./shopping-list.component.css",
+    "../../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css"
   ]
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
+  task: string;
+  tasks = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  onClick() {
+    this.tasks.push({ name: this.task });
+    this.task = "";
   }
-
 }
