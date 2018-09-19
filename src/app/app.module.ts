9697f861fdb1841669cbd3bd94f005362085bc8b
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-19 01:30:44 -0700

    Adds FontAwesome dependencies, icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen, faTimesCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add( faPlusCircle, faPen, faTimesCircle );
+
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlusCircle, faPen, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  faPlusCircle = faPlusCircle;
+  faPen = faPen;
+  faTimesCircle = faTimesCircle;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
